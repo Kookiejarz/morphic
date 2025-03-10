c5d1770e2d2e97facff081ee51fc590994e36c9d
Kenneth! <121078488+Kookiejarz@users.noreply.github.com>
2025-03-11 07:19:59 +0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -22,7 +22,7 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://github.com/Kookiejarz/morphic" target="_blank">
+          <Link href="https://github.com/Kookiejarz" target="_blank">
             <SiGithub size={18} />
           </Link>
         </Button>
