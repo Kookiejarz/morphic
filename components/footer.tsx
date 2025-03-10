b037fd0baff0a2fd2c324001d027a2a599e80d7a
Kenneth! <121078488+Kookiejarz@users.noreply.github.com>
2025-03-11 07:14:00 +0800

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link'
 import React from 'react'
-import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
+import { SiDiscord, SiGithub } from 'react-icons/si'
+import { RiLinkUnlinkM } from "react-icons/ri"
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
@@ -12,8 +13,8 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
+          <Link href="https://www.instagram.com/kennethhhliu" target="_blank">
+            <SiInstagram size={18} />
           </Link>
         </Button>
         <Button
@@ -21,8 +22,8 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://x.com/morphic_ai" target="_blank">
-            <SiX size={18} />
+          <Link href="https://github.com/Kookiejarz/morphic" target="_blank">
+            <SiGithub size={18} />
           </Link>
         </Button>
         <Button
@@ -30,7 +31,7 @@ const Footer: React.FC = () => {
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://git.new/morphic" target="_blank">
+          <Link href="https://github.com/Kookiejarz/morphic" target="_blank">
             <SiGithub size={18} />
           </Link>
         </Button>
